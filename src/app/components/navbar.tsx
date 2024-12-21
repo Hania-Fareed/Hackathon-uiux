10907775b7353919975fb0ec7c73c6d38980b7cb
Hania-Fareed <haniafareed7@gmail.com>
2024-12-21 16:21:00 +0500

    responsive

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,15 +1,57 @@
-import Link from "next/link"
-export default function Contacts(){
-    return(
-        <div>
-            <ul className="flex items-center gap-9 bg-purple-600 h-10 text-white">
-                <Link className="hover:text-black transition-all duration-300" href={"/"}><li className="ml-96">haniafareed7@gmail.com</li></Link>
-                <Link className="hover:text-black transition-all duration-300" href={"/"}><li className="mr-64">(0333)3538055</li></Link>
-                <Link className="hover:text-black transition-all duration-300" href={"/"}><li className="ml-96 ">English</li></Link>
-                <Link className="hover:text-black transition-all duration-300" href={"/"}><li>USD</li></Link>  
-                <Link className="hover:text-black transition-all duration-300" href={"/Login"}><li>Login</li></Link>
-                <Link className="hover:text-black transition-all duration-300" href={"/"}><li>Wishlist</li></Link>
-            </ul>
-        </div>
-    )
+import Link from "next/link";
+
+export default function Contacts() {
+  return (
+    <div className="bg-purple-600 text-white">
+      <ul className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-9 p-4">
+        {/* Email */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/"
+        >
+          <li>haniafareed7@gmail.com</li>
+        </Link>
+
+        {/* Phone */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/"
+        >
+          <li>(0333) 3538055</li>
+        </Link>
+
+        {/* Language */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/"
+        >
+          <li>English</li>
+        </Link>
+
+        {/* Currency */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/"
+        >
+          <li>USD</li>
+        </Link>
+
+        {/* Login */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/Login"
+        >
+          <li>Login</li>
+        </Link>
+
+        {/* Wishlist */}
+        <Link
+          className="hover:text-black transition-all duration-300"
+          href="/"
+        >
+          <li>Wishlist</li>
+        </Link>
+      </ul>
+    </div>
+  );
 }
