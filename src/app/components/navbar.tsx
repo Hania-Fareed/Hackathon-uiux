e608a5d60c6528946ca7836e0310c11d7799cdb7
Hania-Fareed <haniafareed7@gmail.com>
2024-12-21 17:23:08 +0500

    responsive

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,55 +2,54 @@ import Link from "next/link";
 
 export default function Contacts() {
   return (
-    <div className="bg-purple-600 text-white">
-      <ul className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-9 p-4">
-        {/* Email */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/"
-        >
-          <li>haniafareed7@gmail.com</li>
-        </Link>
+    <div className="bg-purple-600 text-white px-4">
+      <ul className="flex items-center justify-between px-96 h-10">
+        {/* Left Section: Email and Phone */}
+        <div className="flex items-center gap-4">
+          <Link
+            className="flex items-center gap-1 hover:text-black transition-all duration-300"
+            href="/"
+          >
+            <span>📧</span> {/* Email Icon */}
+            <li>mhhasanul@gmail.com</li>
+          </Link>
 
-        {/* Phone */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/"
-        >
-          <li>(0333) 3538055</li>
-        </Link>
+          <Link
+            className="flex items-center gap-1 hover:text-black transition-all duration-300"
+            href="/"
+          >
+            <span>📞</span> {/* Phone Icon */}
+            <li>(12345)67890</li>
+          </Link>
+        </div>
 
-        {/* Language */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/"
-        >
-          <li>English</li>
-        </Link>
-
-        {/* Currency */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/"
-        >
-          <li>USD</li>
-        </Link>
-
-        {/* Login */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/Login"
-        >
-          <li>Login</li>
-        </Link>
-
-        {/* Wishlist */}
-        <Link
-          className="hover:text-black transition-all duration-300"
-          href="/"
-        >
-          <li>Wishlist</li>
-        </Link>
+        {/* Right Section: Language, Currency, Login, Wishlist */}
+        <div className="flex items-center gap-4">
+          <Link
+            className="hover:text-black transition-all duration-300"
+            href="/"
+          >
+            <li>English</li>
+          </Link>
+          <Link
+            className="hover:text-black transition-all duration-300"
+            href="/"
+          >
+            <li>USD</li>
+          </Link>
+          <Link
+            className="hover:text-black transition-all duration-300"
+            href="/Login"
+          >
+            <li>Login</li>
+          </Link>
+          <Link
+            className="hover:text-black transition-all duration-300"
+            href="/"
+          >
+            <li>Wishlist</li>
+          </Link>
+        </div>
       </ul>
     </div>
   );
